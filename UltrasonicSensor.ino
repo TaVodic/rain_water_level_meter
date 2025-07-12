07587e06dfd144e801f9dbea6b3dab670fdbeb9a
Martin Ťavoda <martin.tavoda@gmail.com>
2025-07-12 19:06:27 +0200

    RF channel testing

--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -1,13 +1,15 @@
 #include <EEPROM.h>
+#include <RH_ASK.h>
 
 #include "Arduino.h"  // avr core
 
-#define TRIG_PIN 9
-#define ECHO_PIN 10
+#define TRIG_PIN 6
+#define ECHO_PIN 7
 
 #define AVG_COUNT 10
 
-// float duration, distance;
+// uint16_t speed = 2000, uint8_t rxPin = 11, uint8_t txPin = 12, uint8_t pttPin = 10, bool pttInverted = false
+RH_ASK rf_driver(500, 11, 12, 10, false);
 
 uint16_t measure() {
   digitalWrite(TRIG_PIN, LOW);
@@ -40,14 +42,14 @@ void data_colection() {
     Serial.print(duration_us);
     Serial.print(" ");
     dur_sum = dur_sum + duration_us;
-    delay(500);
+    delay(5);
     upload_data();
   }
 
   duration_us = dur_sum / 10;
 
   Serial.print("\nDuration avg: ");
-  Serial.print(duration_us);
+  Serial.println(duration_us);
 
   float distance = (duration_us * .0343) / 2;
   Serial.print(" Distance avg: ");
@@ -58,6 +60,12 @@ void data_colection() {
   EEPROM.put(addr, duration_us);
   addr = addr + sizeof(duration_us);
   EEPROM.put(0, addr);
+
+  uint8_t msg[2];
+  msg[0] = duration_us;
+  msg[1] = duration_us >> 8;
+  rf_driver.send(msg, 2);
+  rf_driver.waitPacketSent();
 }
 
 void upload_data() {
@@ -70,16 +78,18 @@ void upload_data() {
         Serial.println(data);
       }
       Serial.println("Upload completed!\n");
-      while (1){};
+      while (1) {
+      };
     }
     if (inChar == '.') {
       for (uint16_t i = 2; i < EEPROM.length(); i++) {
         uint8_t data = 0;
-        EEPROM.put(i, data);        
+        EEPROM.put(i, data);
       }
       EEPROM.put(0, (uint16_t)2);
       Serial.println("Erase completed!");
-      while (1){};
+      while (1) {
+      };
     }
   }
 }
@@ -89,20 +99,38 @@ void setup() {
   pinMode(ECHO_PIN, INPUT);
   Serial.begin(115200);
 
-  uint32_t c_time = millis();
-  while(c_time + 2000 > millis()){
+  /*uint32_t c_time = millis();
+  while (c_time + 2000 > millis()) {
     upload_data();
-  }  
+  }*/
 
-  //EEPROM.put(0, 2);  // store address 2 at adress 0
+  rf_driver.init();
+
+  // EEPROM.put(0, 2);  // store address 2 at adress 0
 }
 
 void loop() {
-  uint32_t c_time = 0;
+  /*static uint32_t c_time = 0;
   if (c_time + 1000 < millis()){
     data_colection();
     c_time = millis();
+  }*/
+
+  // upload_data();
+
+  static uint32_t c_time = 0;
+  static uint16_t duration_us;
+  if (c_time + 1000 < millis()) {
+    uint8_t msg[2];
+    msg[0] = duration_us;
+    msg[1] = duration_us >> 8;
+    rf_driver.send(msg, 2);
+    Serial.println("Sent");
+    Serial.println(duration_us);
+    //rf_driver.waitPacketSent();
+    duration_us++;
+    c_time = millis();
   }
-  
-  upload_data();
+  // delay(100);
+  // rf_driver.mode() == rf_driver.RHModeTx;
 }
