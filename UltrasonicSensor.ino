ec9a11d36a0071b971a65f89dd71b28abdd1d130
Martin Ťavoda <martin.tavoda@gmail.com>
2025-07-12 22:27:02 +0200

    before sleep integration

--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -6,7 +6,9 @@
 #define TRIG_PIN 6
 #define ECHO_PIN 7
 
-#define AVG_COUNT 10
+#define AVG_COUNT         10
+#define TIME_BETWEEN_MEAS 500
+// #define EEPROM
 
 // uint16_t speed = 2000, uint8_t rxPin = 11, uint8_t txPin = 12, uint8_t pttPin = 10, bool pttInverted = false
 RH_ASK rf_driver(500, 11, 12, 10, false);
@@ -20,30 +22,31 @@ uint16_t measure() {
   return (uint16_t)pulseIn(ECHO_PIN, HIGH);
 }
 
-void data_colection() {
+uint16_t data_colection() {
+#ifdef EEPROM
   static uint16_t addr;
   static uint8_t firstRun = true;
-
   if (firstRun) {
     EEPROM.get(0, addr);  // Read 2 bytes into addr
     firstRun = false;
   }
-
   if (addr >= EEPROM.length()) {
     Serial.println("EEPROM full!");
     return;
   }
+#endif
 
   uint16_t duration_us;
   uint32_t dur_sum = 0;
-  Serial.print("Dur frag: ");
+
+  Serial.print("Duration ms sample: ");
   for (uint8_t i = 0; i < AVG_COUNT; i++) {
     duration_us = measure();
     Serial.print(duration_us);
     Serial.print(" ");
     dur_sum = dur_sum + duration_us;
-    delay(5);
-    upload_data();
+    delay(TIME_BETWEEN_MEAS);  // TODO: go to sleep between measurements
+    // upload_data();
   }
 
   duration_us = dur_sum / 10;
@@ -51,21 +54,24 @@ void data_colection() {
   Serial.print("\nDuration avg: ");
   Serial.println(duration_us);
 
-  float distance = (duration_us * .0343) / 2;
-  Serial.print(" Distance avg: ");
-  Serial.println(distance, 1);
-
+#ifdef EEPROM
   Serial.print("Writing on address: ");
   Serial.println(addr);
   EEPROM.put(addr, duration_us);
   addr = addr + sizeof(duration_us);
   EEPROM.put(0, addr);
+#endif
 
+  return duration_us;
+}
+
+void send_packet(uint16_t duration_us) {
   uint8_t msg[2];
   msg[0] = duration_us;
   msg[1] = duration_us >> 8;
   rf_driver.send(msg, 2);
   rf_driver.waitPacketSent();
+  // rf_driver.mode() == rf_driver.RHModeTx;
 }
 
 void upload_data() {
@@ -110,27 +116,16 @@ void setup() {
 }
 
 void loop() {
-  /*static uint32_t c_time = 0;
-  if (c_time + 1000 < millis()){
-    data_colection();
-    c_time = millis();
-  }*/
-
-  // upload_data();
-
   static uint32_t c_time = 0;
-  static uint16_t duration_us;
-  //if (c_time + 1000 < millis()) {
-    uint8_t msg[2];
-    msg[0] = duration_us;
-    msg[1] = duration_us >> 8;
-    rf_driver.send(msg, 2);
-    Serial.println("Sent");
-    Serial.println(duration_us);
-    rf_driver.waitPacketSent();
-    duration_us++;
+
+  if (c_time + 1000 < millis()) {
+    uint16_t duration_us = data_colection();
+    send_packet(duration_us);
+
+    float distance = (duration_us * .0343) / 2;
+    Serial.print(" Distance avg: ");
+    Serial.println(distance, 1);
+
     c_time = millis();
-  //}
-  // delay(100);
-  // rf_driver.mode() == rf_driver.RHModeTx;
+  }
 }
