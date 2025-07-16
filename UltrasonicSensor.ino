99d736fb137e3bafb4ae7c48af200f644df8e835
Martin Ťavoda <martin.tavoda@gmail.com>
2025-07-16 16:19:56 +0200

    temperature compensated, ICP1 implemented

--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -1,34 +1,54 @@
 #include <EEPROM.h>
 #include <RH_ASK.h>
 #include <avr/io.h>
-#include <avr/sleep.h>
 #include <avr/power.h>
+#include <avr/sleep.h>
+#include <DS3231.h>
+#include <Wire.h>
 
 #include "Arduino.h"  // avr core
 
 #define INT_pin PD2  // change need to be updated in interrupt settings
-#define RF_pin  8
+#define RF_pin  7
 
 #define RF_VCC_pin    4
 #define SONIC_VCC_pin 5
 
 #define TRIG_PIN 6
-#define ECHO_PIN 7
+#define ECHO_PIN 8
 
-#define AVG_COUNT         2    // 10
+#define AVG_COUNT         10   // 10
 #define TIME_BETWEEN_MEAS 500  // 500
 // #define EEPROM
 
 // uint16_t speed = 2000, uint8_t rxPin = 11, uint8_t txPin = 12, uint8_t pttPin = 10, bool pttInverted = false
 RH_ASK rf_driver(500, RF_pin, RF_pin, RF_pin, false);
+DS3231 rtc;
+
+volatile uint16_t start_time = 0;
+volatile uint16_t echo_duration = 0;
+volatile uint8_t capture_done = 0;
 
 uint16_t measure() {
+  TCNT1 = 0;  // Reset timer (optional, but safe)
+  capture_done = 0;
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-  return (uint16_t)pulseIn(ECHO_PIN, HIGH);
+  while (!capture_done);  // Wait for echo
+  return echo_duration*2; // prescaler 8 means 0.5us resolution
+}
+
+void init_ultrasonic_icp1() {
+
+  // Configure Timer1
+  TCCR1A = 0;                           // set to normal mode
+  TCCR1B = (1 << ICES1) | (1 << CS11);  // Rising edge, prescaler = 8 (1 µs resolution)
+  TIMSK1 |= (1 << ICIE1);               // Enable input capture interrupt
+
+  sei();  // Enable global interrupts
 }
 
 uint16_t data_colection() {
@@ -62,6 +82,21 @@ uint16_t data_colection() {
 
   Serial.print("\nDuration avg: ");
   Serial.println(duration_us);
+
+
+  float tempC = rtc.getTemperature();
+
+  Serial.print("T=");
+	Serial.print(tempC, 2);
+
+  float c = 331.3 + 0.606 * tempC; 
+
+  Serial.print("c=");
+	Serial.print(c, 2);
+
+  float distance = (duration_us * c / 10000) / 2;
+  Serial.print(" Distance avg: ");
+  Serial.println(distance, 1);
 
 #ifdef EEPROM
   Serial.print("Writing on address: ");
@@ -149,26 +184,28 @@ void go_to_sleep() {
 }
 
 void setup_clock_prescaler() {
-  cli(); // Disable interrupts during change
+  cli();  // Disable interrupts during change
 
-  CLKPR = (1 << CLKPCE); // Enable change of CLKPR
-  CLKPR = (1 << CLKPS1); // Set prescaler to divide by 16 (16MHz / 16 = 1MHz)
+  CLKPR = (1 << CLKPCE);  // Enable change of CLKPR
+  CLKPR = (1 << CLKPS1);  // Set prescaler to divide by
 
-  sei(); // Re-enable interrupts
+  sei();  // Re-enable interrupts
 }
 
 void setup() {
-  //setup_clock_prescaler();
+  setup_clock_prescaler();
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RF_VCC_pin, OUTPUT);
   pinMode(SONIC_VCC_pin, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);  
-  Serial.begin(115200);
+  pinMode(ECHO_PIN, INPUT);
+  Serial.begin(9600);
 
   rf_driver.init();
+  Wire.begin();
 
   setup_pinchange_interrupt();
+  init_ultrasonic_icp1();
 
   digitalWrite(RF_VCC_pin, HIGH);
   digitalWrite(SONIC_VCC_pin, HIGH);
@@ -177,19 +214,40 @@ void setup() {
 }
 
 void loop() {
-  static uint8_t meas_count;
-  uint16_t duration_us = data_colection();
+  static uint32_t cTime;
+  if (cTime + 2000 < millis()) {
+    static uint8_t meas_count;
+    uint16_t duration_us = data_colection();
 
-  EEPROM.put(2 * meas_count, duration_us);
+    /*EEPROM.put(2 * meas_count, duration_us);
 
-  meas_count++;
-  if (meas_count >= 3) {
-    for (uint8_t i = 0; i < meas_count; i++) {
-      EEPROM.get(2 * i, duration_us);
-      send_packet(duration_us);
-    }
-    meas_count = 0;
+    meas_count++;
+    if (meas_count >= 3) {
+      for (uint8_t i = 0; i < meas_count; i++) {
+        EEPROM.get(2 * i, duration_us);
+        send_packet(duration_us);
+      }
+      meas_count = 0;
+    }*/
+    cTime = millis();
   }
 
-  go_to_sleep();
+  // go_to_sleep();
+}
+
+ISR(TIMER1_CAPT_vect) {
+  static uint8_t state = 0;
+
+  if (state == 0) {
+    // Rising edge detected
+    start_time = ICR1;
+    TCCR1B &= ~(1 << ICES1);  // Switch to falling edge
+    state = 1;
+  } else {
+    // Falling edge detected
+    echo_duration = ICR1 - start_time;
+    TCCR1B |= (1 << ICES1);  // Switch back to rising edge
+    capture_done = 1;
+    state = 0;
+  }
 }
